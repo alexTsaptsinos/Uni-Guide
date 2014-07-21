39964676ac325e5e1575bfafc3383c14ebb1e9d8
Alex Tsaptsinos <alexandros.tsaptsinos@pmb.ox.ac.uk>
2014-07-21 16:39:56 +0100

    Test

--- UniGuide/UniversitiesListTableViewController.m
+++ UniGuide/UniversitiesListTableViewController.m
@@ -89,6 +89,7 @@
     return cell;
 }
 
+//this is just a test
 #pragma mark Content Filtering
 -(void)filterContentForSearchText:(NSString*)searchText {
     //Update the filtered array based on the search text
